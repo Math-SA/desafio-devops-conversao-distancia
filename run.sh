ea1908f2a639d9255c4a0b8013a1138a1a8ec52d
Math-SA <matheus.s.assis@gmail.com>
2025-01-13 13:59:40 -0300

    Adicionei script run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+if ! docker image ls| cut --delimiter " " --fields 1-4 | grep "conversao-distancia   v1_dev" ; then
+    docker build -t conversao-distancia:v1_dev .
+fi
+
+porta_5000=$(docker container ls | cut -s --delimiter " " --fields 1,22 | grep "0.0.0.0:5000->")
+if [[ -n $porta_5000 ]] ; then
+    container=$(echo "$porta_5000" | cut -s --delimiter " " --fields 1)
+    echo "Container $container utilizando a porta 5000. Terminando execução em 5 segundos. Ctrl+C para cancelar."
+    sleep 5
+    docker container stop $container
+fi
+
+docker container run -d -p 5000:5000 conversao-distancia:v1_dev
+
+if curl -fs --retry 15 --retry-max-time 120 --retry-all-errors http://localhost:5000 > /dev/null; then
+   echo "Container iniciado com sucesso: http://localhost:5000"
+fi;
